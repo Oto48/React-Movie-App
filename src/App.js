bb1b9dba831e57e9dce39d9f076d1fa21b5f283a
Oto Tchokhonelidze <oto.tchokhonelidze@programmingo.com>
2023-07-10 01:12:06 +0400

    add react router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Movies from './components/Movies/Movies';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div>
-      <Navbar />
-      <h1>Trending Movies and TV Shows</h1>
-      <Movies endpoint={'trending'} />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <h1>Trending Movies and TV Shows</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/movies" />}
+          />
+          <Route path="/movies" element={<Movies endpoint={'movie'} />} />
+          <Route path="/tv" element={<Movies endpoint={'tv'} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
-const Movies = ({ endpoint, titleKey }) => {
+const Movies = ({ endpoint }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchMedia = async () => {
       try {
         const apiKey = 'f5f42920d31e693ff3c7c36e87e03dd4';
-        const response = await fetch(`https://api.themoviedb.org/3/${endpoint}/all/day?language=en-US&api_key=${apiKey}&page=${1}`);
+        const response = await fetch(`https://api.themoviedb.org/3/discover/${endpoint}?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`);
         const data = await response.json();
         setMovies(data.results);
       } catch (error) {
@@ -20,7 +20,6 @@ const Movies = ({ endpoint, titleKey }) => {
 
   return (
     <div>
-      <h1>{titleKey}</h1>
       <ul>
         {movies.map((movie) => (
           <li key={movie.id}>{movie.title || movie.name}</li>
