28c94526a2c7ebfc83e2eeadb326282ff223b6d4
Oto Tchokhonelidze <oto.tchokhonelidze@programmingo.com>
2023-09-08 16:16:57 +0400

    change database

--- back-end/db/connection.js
+++ back-end/db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const mongoDBUrl = "mongodb://localhost:27017/movie_app";
+const mongoDBUrl =
+  "mongodb+srv://user:user123@cluster0.djbyzkh.mongodb.net/?retryWrites=true&w=majority";
 
 // Return a promise that resolves when the connection is open
 const connectToDatabase = () => {
